ab8ae108ea8ed2e1fb26cc18965414abfef5eae2
jni <jni@users.noreply.github.com>
2024-11-13 22:48:40 +0000

    deploy: napari/docs@142ac9a29857a5db5fcc5c3960c3d4b2c1c83073

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.4rc4.dev25+gd05d688c',
+    VERSION: '0.5.4rc4.dev27+g33da488d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
