6c7625fd07e1a62e481e534d996d83167d6b8129
jni <jni@users.noreply.github.com>
2025-01-22 09:17:08 +0000

    deploy: napari/docs@e70c1e226e3cc637b12208020bb678099c44a0bc

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.6rc1.dev3+g74dc2e4e',
+    VERSION: '0.5.6rc2.dev1+g42a6b9eb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
